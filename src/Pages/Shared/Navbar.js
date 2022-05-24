2f504e02716b334f00c1be639748e0b236ed0e09
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-24 12:54:33 +0600

    Environment Variable Implementaion Successfully Added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,12 +1,23 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Navbar = () => {
+
+    const [user] = useAuthState(auth);
+
+    const logout = () => {
+        signOut(auth);
+
+    };
+
+
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-
+        <li>{user ? <button onClick={logout} className="btn  btn-ghost">LogOut</button> : <Link to='/login'>Login</Link>}</li>
 
     </>
 
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDLVhXanhiSBmv2DRGAuqyKDEC6yBIPdBw",
-    authDomain: "manufacturer-tools-f767c.firebaseapp.com",
-    projectId: "manufacturer-tools-f767c",
-    storageBucket: "manufacturer-tools-f767c.appspot.com",
-    messagingSenderId: "583888869216",
-    appId: "1:583888869216:web:2811af893edfd45d947005"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
