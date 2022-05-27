df70bc0b2cf9839388bd5349c3c714c5c5bd9964
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-27 12:13:08 +0600

    MY Orders Data Displeyed in the dashboard

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?client=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+    }, [user])
     return (
         <div>
-            <h2>orders</h2>
+            <h2>orders: {orders.length}</h2>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Product Name</th>
+                            <th>Order Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order =>
+                                <tr>
+                                    <th>1</th>
+                                    <td>{order.clientName}</td>
+                                    <td>{order.product}</td>
+                                    <td>{order.quantity}</td>
+                                </tr>)
+                        }
+
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
